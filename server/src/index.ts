f5a60814c23f2fd2bb7f733ad4554bd77cb20428
Timilehin Odulate <timiodulate@gmail.com>
2026-03-31 22:07:26 +0100

    chore: adds socket.io

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,23 @@
 import express, { Request, Response } from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
+
 const PORT = process.env.PORT || 3002;
+
+const server = http.createServer(app);
+// const httpServer = createServer();
+const io = new Server(server, {
+	cors: {
+		origin: process.env.CLIENT_URL || "http://localhost:3000",
+		methods: ["GET", "POST"],
+	},
+});
+
+io.on("connection", (socket) => {
+	// ...
+});
 
 // app.use(express.json());
 
@@ -9,6 +25,6 @@ app.get("/", (req: Request, res: Response) => {
 	res.send("Hello, TypeScript + Express!");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
